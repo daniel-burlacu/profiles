1248902f0a35d9b80bb47ebcc6d8a0c12b4ab96c
Daniel Burlacu <daniel.burlacu1983@yahoo.se>
2022-11-06 20:31:11 +0100

    adding audit files

--- migrations/1-athlete-deployment.js
+++ migrations/1-athlete-deployment.js
@@ -1,5 +1,5 @@
 const Athlete = artifacts.require("Athlete");
 
 module.exports = function(deployer,accounts){
-    deployer.deploy(Athlete,'Athlete','ATHL','https://behindmaskssociety.com',{from:'0x9D9d831Dc8E2734018F211992C35779Ef615eA1F'});
+    deployer.deploy(Athlete,'Athlete','ATHL','https://behindmaskssociety.com',{from:'0x760DE429EF0FE3C25C03Da388401d246CE668505'});
 }
